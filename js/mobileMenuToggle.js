ac664a9ca5e1ad66d1367085b8587f0fac5e1654
yebinable <yebinable@kakao.com>
2025-04-19 11:10:05 +0900

    블로그 글 업데이트

--- js/mobileMenuToggle.js
+++ js/mobileMenuToggle.js
@@ -1,13 +1,6 @@
-const menuButton = document.getElementById("menu-button");
-const menu = document.getElementById("menu");
-
-/*
-모바일 환경에서 menu, 이 menu는 이벤트 위임으로 최적화하면 불필요한 코드가 많은 함수입니다. 시간상 최적화하지 않고 넘깁니다.
-*/
-const mobileMenu = document.getElementById("mobileMenu");
-
 window.addEventListener("click", (event) => {
     if (event.target === menuButton) {
+        // 기존 코드 유지
         if (mobileMenu.innerHTML === "") {
             mobileMenu.innerHTML = menu.innerHTML;
             const menuItems = mobileMenu.querySelectorAll("a");
@@ -24,7 +17,10 @@ window.addEventListener("click", (event) => {
     } else if (event.target.parentNode === mobileMenu) {
         event.preventDefault();
 
-        if (event.target.innerText + ".md" === "blog.md") {
+        // 수정된 부분: 파일명을 소문자로 변환
+        const filename = (event.target.innerText + ".md").toLowerCase();
+
+        if (filename === "blog.md") {
             if (blogList.length === 0) {
                 // 블로그 리스트 로딩
                 initDataBlogList().then(() => {
@@ -35,11 +31,11 @@ window.addEventListener("click", (event) => {
             }
             // console.log(origin)
             const url = new URL(origin);
-            url.searchParams.set("menu", event.target.innerText + ".md");
+            url.searchParams.set("menu", filename);
             window.history.pushState({}, "", url);
             mobileMenu.innerHTML = "";
         } else {
-            renderOtherContents(event.target.innerText + ".md");
+            renderOtherContents(filename);
             mobileMenu.innerHTML = "";
         }
     } else {
